c51193d8ec6223f005097d891d1efb48d302b39a
Wojciech <thaazar@gmail.com>
2024-06-26 19:26:55 +0200

    dodane JPA oraz mysql-connector utworzenie oraz przetestowanie połączenia z DB w kontenerze dockera.

---
+++ src/main/java/com/wojciechbarwinski/demo/epic_board_games_shop/EntityTestRepository.java
@@ -0,0 +1,6 @@
+package com.wojciechbarwinski.demo.epic_board_games_shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntityTestRepository extends JpaRepository<EntityTest, Long> {
+}
