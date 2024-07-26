248083a38a9a754c9200c70984e21e9511fb011e
Wojciech <thaazar@gmail.com>
2024-07-26 19:39:06 +0200

    ES-005.Tests
    Tests

--- src/main/java/com/wojciechbarwinski/demo/epic_board_games_shop/entities/Product.java
+++ src/main/java/com/wojciechbarwinski/demo/epic_board_games_shop/entities/Product.java
@@ -3,7 +3,6 @@ package com.wojciechbarwinski.demo.epic_board_games_shop.entities;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 
 import java.math.BigDecimal;
 
