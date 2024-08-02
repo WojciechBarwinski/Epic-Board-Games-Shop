3006e3ce2c09ba0bfb0ce49cc8bda052d5165091
Wojciech <thaazar@gmail.com>
2024-08-02 13:16:09 +0200

    ES-005.MapStructure
    add mapStructure to project

--- src/main/java/com/wojciechbarwinski/demo/epic_board_games_shop/mappers/MapperFacade.java
+++ src/main/java/com/wojciechbarwinski/demo/epic_board_games_shop/mappers/MapperFacade.java
@@ -4,17 +4,16 @@ package com.wojciechbarwinski.demo.epic_board_games_shop.mappers;
 import com.wojciechbarwinski.demo.epic_board_games_shop.dtos.CreateOrderRequestDTO;
 import com.wojciechbarwinski.demo.epic_board_games_shop.dtos.OrderResponseDTO;
 import com.wojciechbarwinski.demo.epic_board_games_shop.entities.Order;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MapperFacade {
 
-    public Order mapCreateOrderRequestDTOToOrderEntity(CreateOrderRequestDTO createOrderRequestDto){
+    public Order mapCreateOrderRequestDTOToOrderEntity(CreateOrderRequestDTO createOrderRequestDto) {
         return OrderMapperTest.INSTANCE.mapCreateOrderRequestDTOToOrder(createOrderRequestDto);
     }
 
-    public OrderResponseDTO mapOrderToOrderResponseDTO(Order order){
+    public OrderResponseDTO mapOrderToOrderResponseDTO(Order order) {
         return OrderMapperTest.INSTANCE.mapOrderToResponseOrderDTO(order);
     }
 }
